2d39d500e5cd0882b043915fd1fc62bf82fc7d8e
Hoobzhu <qinhezhu@yeah.net>
2020-05-15 11:52:53 +0800

    监控聚合服务turbine

---
+++ microservice-turbine/src/main/java/com/turbine/cloud/TurbineApplication.java
@@ -0,0 +1,15 @@
+package com.turbine.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@EnableTurbine
+@EnableDiscoveryClient
+@SpringBootApplication
+public class TurbineApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(TurbineApplication.class, args);
+  }
+}
